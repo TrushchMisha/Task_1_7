f2b096f380a52e781dbb8afa362083a7a9c575ac
TrushchMisha <43968047+TrushchMisha@users.noreply.github.com>
2018-11-08 15:43:07 +0200

    Task

--- src/main/java/tm/SecondTask.java
+++ src/main/java/tm/SecondTask.java
@@ -3,6 +3,7 @@ package tm;
 public class SecondTask {
     public static void main(String[] args){
         int a = 2;
+
         while(a < 10000){
             System.out.println(a);
             a = 2 * a - 1;
